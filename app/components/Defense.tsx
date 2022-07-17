581c92bfe769990393225b244dbea8be5552f3f7
Ryan McCarthy <rmmcc92@aol.com>
2022-07-16 20:44:32 -0400

    Defense skeleton added

--- app/components/Defense.tsx
+++ app/components/Defense.tsx
@@ -1,4 +1,11 @@
-import { Center } from "@mantine/core";
+import {
+  Center,
+  Container,
+  Paper,
+  TextInput,
+  Text,
+  Button,
+} from "@mantine/core";
 
 export default function Defense() {
   return (
@@ -10,7 +17,423 @@ export default function Defense() {
         margin: 0,
       }}
     >
-      <h1>Defense</h1>
+      <Container>
+        <h1>Defense</h1>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>AC</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="Armor Bonus" />
+          <TextInput description="Shield Bonus" />
+          <TextInput description="Dex Modifier" />
+          <TextInput description="Size Modifier" />
+          <TextInput description="Natural Armor" />
+          <TextInput description="Deflection" />
+          <TextInput description="Misc" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Touch</Text>
+          </Paper>
+          <TextInput />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Flat Footed</Text>
+          </Paper>
+
+          <div style={{ display: "flex", alignItems: "flex-end" }}>
+            <TextInput />
+            <TextInput description="Other AC Modifiers" />
+          </div>
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>HP</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="Wounds/Current" />
+          <TextInput description="Non-Lethal" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>DR</Text>
+          </Paper>
+          <TextInput />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>SR</Text>
+          </Paper>
+          <TextInput />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Fortitude</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="Base" />
+          <TextInput description="Ability Modifier" />
+          <TextInput description="Magic Modifier" />
+          <TextInput description="Misc" />
+          <TextInput description="Temp" />
+          <TextInput description="Other" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Reflex</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="Base" />
+          <TextInput description="Ability Modifier" />
+          <TextInput description="Magic Modifier" />
+          <TextInput description="Misc" />
+          <TextInput description="Temp" />
+          <TextInput description="Other" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Will</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="Base" />
+          <TextInput description="Ability Modifier" />
+          <TextInput description="Magic Modifier" />
+          <TextInput description="Misc" />
+          <TextInput description="Temp" />
+          <TextInput description="Other" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>DR</Text>
+          </Paper>
+          <TextInput />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Resist</Text>
+          </Paper>
+          <TextInput description="Elemental & Other" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>Imune</Text>
+          </Paper>
+          <TextInput description="Effects & Other" />
+        </Container>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "1%",
+            marginBottom: "1%",
+          }}
+        >
+          <Paper
+            shadow="md"
+            radius="sm"
+            withBorder
+            style={{
+              backgroundColor: `black`,
+              padding: `1%`,
+              color: `white`,
+              height: `95%`,
+              alignSelf: `flex-end`,
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+              alignItems: `center`,
+              marginRight: `1%`,
+            }}
+          >
+            <Text>CMD</Text>
+          </Paper>
+          <TextInput description="Total" />
+          <TextInput description="BAB" />
+          <TextInput description="Strength Modifier" />
+          <TextInput description="Dex Modifier" />
+          <TextInput description="Size Modifier" />
+          <TextInput description="Misc" />
+          <TextInput description="Temp" />
+        </Container>
+      </Container>
     </Center>
   );
 }
