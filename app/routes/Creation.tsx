9ce69a10bb4e5c24697b59c6e4bd51b802bd9d5c
Ryan McCarthy <rmmcc92@aol.com>
2022-08-01 20:22:45 -0400

    added more forms in creation

--- app/routes/Creation.tsx
+++ app/routes/Creation.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Modal from "~/components/Modal";
 import { Container, Title, Text, Button, Stepper } from "@mantine/core";
 import { Form, useActionData } from "@remix-run/react";
 import { FormField } from "~/components/form-field";
@@ -14,6 +13,24 @@ export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   const characterName = form.get("characterName");
   const characterClass = form.get("characterClass");
+  const alignment = form.get("alignment");
+  const level = form.get("level");
+  const deity = form.get("deity");
+  const homeland = form.get("homeland");
+  const race = form.get("race");
+  const size = form.get("size");
+  const gender = form.get("gender");
+  const characterAge = form.get("characterAge");
+  const characterHeight = form.get("characterHeight");
+  const weight = form.get("weight");
+  const hairColor = form.get("hairColor");
+  const eyeColor = form.get("eyeColor");
+  const strength = form.get("strength");
+  const dexterity = form.get("dexterity");
+  const constitution = form.get("constitution");
+  const intelligence = form.get("intelligence");
+  const wisdom = form.get("wisdom");
+  const charisma = form.get("charisma");
 };
 const Creation = () => {
   //   const { user } = useLoaderData();
@@ -22,6 +39,24 @@ const Creation = () => {
   const [formData, setFormData] = useState({
     characterName: actionData?.fields.characterName || "",
     characterClass: actionData?.fields.characterClass || "",
+    alignment: actionData?.fields.alignment || "",
+    level: actionData?.fields.level || "",
+    deity: actionData?.fields.deity || "",
+    homeland: actionData?.fields.homeland || "",
+    race: actionData?.fields.race || "",
+    size: actionData?.fields.size || "",
+    gender: actionData?.fields.gender || "",
+    characterAge: actionData?.fields.characterAge || "",
+    characterHeight: actionData?.fields.characterHeight || "",
+    weight: actionData?.fields.weight || "",
+    hairColor: actionData?.fields.hairColor || "",
+    eyeColor: actionData?.fields.eyeColor || "",
+    strength: actionData?.fields.strength || "",
+    dexterity: actionData?.fields.dexterity || "",
+    constitution: actionData?.fields.constitution || "",
+    intelligence: actionData?.fields.intelligence || "",
+    wisdom: actionData?.fields.wisdom || "",
+    charisma: actionData?.fields.charisma || "",
   });
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -107,44 +142,229 @@ const Creation = () => {
                 }}
               >
                 {active === 0 ? (
-                  <Form action="/create" method="post">
-                    <FormField
-                      htmlFor="characterName"
-                      label="Character Name"
-                      value={formData.characterName}
-                      type="text"
-                      style={{ width: "100%" }}
-                      onChange={(e) => handleChange(e, "characterName")}
-                    />
-                    <FormField
-                      htmlFor="characterClass"
-                      label="Class"
-                      value={formData.characterClass}
-                      type="text"
-                      style={{ width: "100%" }}
-                      onChange={(e) => handleChange(e, "characterClass")}
-                    />
-                  </Form>
+                  <>
+                    <Title color="white" align="center">
+                      General Info
+                    </Title>
+                    <Form>
+                      <FormField
+                        htmlFor="characterName"
+                        label="Character Name"
+                        value={formData.characterName}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "characterName")}
+                      />
+                      <FormField
+                        htmlFor="alignment"
+                        label="Alignment"
+                        value={formData.alignment}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "alignment")}
+                      />
+                      <FormField
+                        htmlFor="level"
+                        label="Level"
+                        value={formData.level}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "level")}
+                      />
+                      <FormField
+                        htmlFor="deity"
+                        label="Deity"
+                        value={formData.deity}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "deity")}
+                      />
+                      <FormField
+                        htmlFor="homeland"
+                        label="Homeland"
+                        value={formData.homeland}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "homeland")}
+                      />
+                      <FormField
+                        htmlFor="race"
+                        label="Race"
+                        value={formData.race}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "race")}
+                      />
+                      <FormField
+                        htmlFor="size"
+                        label="Size"
+                        value={formData.size}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "size")}
+                      />
+                      <FormField
+                        htmlFor="gender"
+                        label="Gender"
+                        value={formData.gender}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "gender")}
+                      />
+                      <FormField
+                        htmlFor="characterAge"
+                        label="Age"
+                        value={formData.characterAge}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "characterAge")}
+                      />
+                      <FormField
+                        htmlFor="characterHeight"
+                        label="Height"
+                        value={formData.characterHeight}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "characterHeight")}
+                      />
+                      <FormField
+                        htmlFor="weight"
+                        label="Weight"
+                        value={formData.weight}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "weight")}
+                      />
+                      <FormField
+                        htmlFor="hairColor"
+                        label="Hair Color"
+                        value={formData.hairColor}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "hairColor")}
+                      />
+                      <FormField
+                        htmlFor="eyeColor"
+                        label="Eye Color"
+                        value={formData.eyeColor}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "eyeColor")}
+                      />
+                      <FormField
+                        htmlFor="characterClass"
+                        label="Class"
+                        value={formData.characterClass}
+                        type="text"
+                        style={{ width: "100%" }}
+                        onChange={(e) => handleChange(e, "characterClass")}
+                      />
+                    </Form>
+                  </>
                 ) : active === 1 ? (
-                  <div>
-                    <h1>Step 2</h1>
-                  </div>
+                  <>
+                    <Title color="white" align="center">
+                      Stats
+                    </Title>
+                    <Form>
+                      <Container
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          width: "100%",
+                        }}
+                      >
+                        <Container
+                          style={{
+                            width: "50%",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <FormField
+                            htmlFor="strength"
+                            label="Strength"
+                            value={formData.strength}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "strength")}
+                          />
+                          <FormField
+                            htmlFor="dexterity"
+                            label="Dexterity"
+                            value={formData.dexterity}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "dexterity")}
+                          />
+                          <FormField
+                            htmlFor="constitution"
+                            label="Constitution"
+                            value={formData.constitution}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "constitution")}
+                          />
+                        </Container>
+                        <Container
+                          style={{
+                            width: "50%",
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <FormField
+                            htmlFor="intelligence"
+                            label="Intelligence"
+                            value={formData.intelligence}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "intelligence")}
+                          />
+                          <FormField
+                            htmlFor="wisdom"
+                            label="Wisdom"
+                            value={formData.wisdom}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "wisdom")}
+                          />
+                          <FormField
+                            htmlFor="charisma"
+                            label="Charisma"
+                            value={formData.charisma}
+                            type="number"
+                            style={{ width: "51%" }}
+                            onChange={(e) => handleChange(e, "charisma")}
+                          />
+                        </Container>
+                      </Container>
+                    </Form>
+                  </>
                 ) : active === 2 ? (
-                  <div>
-                    <h1>Step 3</h1>
-                  </div>
+                  <>
+                    <Title color="white" align="center">
+                      Skills
+                    </Title>
+                  </>
                 ) : active === 3 ? (
-                  <div>
-                    <h1>Step 4</h1>
-                  </div>
+                  <>
+                    <Title color="white" align="center">
+                      Feats and Traits
+                    </Title>
+                  </>
                 ) : active === 4 ? (
-                  <div>
-                    <h1>Step 5</h1>
-                  </div>
+                  <>
+                    <Title color="white" align="center">
+                      Backstory
+                    </Title>
+                  </>
                 ) : active === 5 ? (
-                  <div>
-                    <h1>Step 6</h1>
-                  </div>
+                  <>
+                    <Title color="white" align="center">
+                      Review
+                    </Title>
+                  </>
                 ) : null}
                 {active < 5 ? (
                   <Button
