0b9ec159fde055cac58672450a1754aa5c8d8687
Ryan McCarthy <rmmcc92@aol.com>
2022-08-05 18:44:40 -0400

    formData making it to the backend complete

--- app/routes/Creation.tsx
+++ app/routes/Creation.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import type { ChangeEvent } from "react";
 import { Container, Title, Text, Button, Stepper } from "@mantine/core";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useSubmit } from "@remix-run/react";
 import { FormField } from "~/components/form-field";
 import Layout from "~/components/Layout";
 // import { useLoaderData } from "@remix-run/react";
@@ -11,30 +11,29 @@ import { json } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
 import { createCharacter } from "~/utils/character.server";
 import type { CharacterForm } from "~/utils/types.server";
-import { generateId } from "~/utils/helpers.server";
-import { saveToRedis } from "~/utils/redis.server";
 
 export const action: ActionFunction = async ({ request }) => {
   // TODO: Stepper is breaking form data will need to submit data per step
   const form = await request.formData();
+  // console.log(form);
   // This will work to clean up some variables Stepper still break it
-  // const values = Object.fromEntries(form);
-  // console.log("values", values);
-  const action = form.get("_action");
-  const characterName = form.get("characterName");
-  const characterClass = form.get("characterClass");
-  const alignment = form.get("alignment");
-  const level = form.get("level");
-  const deity = form.get("deity");
-  const homeland = form.get("homeland");
-  const race = form.get("race");
-  const size = form.get("size");
-  const gender = form.get("gender");
-  const characterAge = form.get("characterAge");
-  const characterHeight = form.get("characterHeight");
-  const weight = form.get("weight");
-  const hairColor = form.get("hairColor");
-  const eyeColor = form.get("eyeColor");
+  const values = Object.fromEntries(form);
+  console.log("values", values);
+  // const action = form.get("_action");
+  // const characterName = form.get("characterName");
+  // const characterClass = form.get("characterClass");
+  // const alignment = form.get("alignment");
+  // const level = form.get("level");
+  // const deity = form.get("deity");
+  // const homeland = form.get("homeland");
+  // const race = form.get("race");
+  // const size = form.get("size");
+  // const gender = form.get("gender");
+  // const characterAge = form.get("characterAge");
+  // const characterHeight = form.get("characterHeight");
+  // const weight = form.get("weight");
+  // const hairColor = form.get("hairColor");
+  // const eyeColor = form.get("eyeColor");
   // const strength = form.get("strength");
   // const dexterity = form.get("dexterity");
   // const constitution = form.get("constitution");
@@ -46,73 +45,81 @@ export const action: ActionFunction = async ({ request }) => {
   //   typeof characterName !== "string" ||
   //   typeof characterClass !== "string" ||
   //   typeof alignment !== "string" ||
-  //   typeof level !== "number" ||
+  //   typeof level !== "string" ||
   //   typeof deity !== "string" ||
   //   typeof homeland !== "string" ||
   //   typeof race !== "string" ||
   //   typeof size !== "string" ||
   //   typeof gender !== "string" ||
   //   typeof characterAge !== "string" ||
-  //   typeof weight !== "string" ||
-  //   typeof strength !== "number" ||
-  //   typeof dexterity !== "number" ||
-  //   typeof constitution !== "number" ||
-  //   typeof wisdom !== "number" ||
-  //   typeof intelligence !== "number" ||
-  //   typeof charisma !== "number" ||
-  //   typeof characterHeight !== "string" ||
-  //   typeof hairColor !== "string" ||
-  //   typeof eyeColor !== "string"
+  //   typeof weight !== "string"
   // ) {
   //   return json({ error: "Invalid form data" }, { status: 400 });
   // }
-  const id = generateId(8);
+  // if (
+  //   action === "step2" &&
+  //   (typeof strength !== "string" ||
+  //     typeof dexterity !== "string" ||
+  //     typeof constitution !== "string" ||
+  //     typeof wisdom !== "string" ||
+  //     typeof intelligence !== "string" ||
+  //     typeof charisma !== "string" ||
+  //     typeof characterHeight !== "string" ||
+  //     typeof hairColor !== "string" ||
+  //     typeof eyeColor !== "string")
+  // ) {
+  //   return json({ error: "Invalid form data" }, { status: 400 });
+  // }
 
-  const formDataObject: CharacterForm = {
-    id,
-    characterName,
-    characterClass,
-    alignment,
-    level,
-    deity,
-    homeland,
-    race,
-    size,
-    gender,
-    characterAge,
-    characterHeight,
-    weight,
-    hairColor,
-    eyeColor,
-  };
-  await saveToRedis(formDataObject);
+  //  This was for Redis but it may not be needed
+  // const id = generateId(8);
+
+  // const formDataObject: CharacterForm = {
+  //   id,
+  //   characterName,
+  //   characterClass,
+  //   alignment,
+  //   level,
+  //   deity,
+  //   homeland,
+  //   race,
+  //   size,
+  //   gender,
+  //   characterAge,
+  //   characterHeight,
+  //   weight,
+  //   hairColor,
+  //   eyeColor,
+  // };
+  // await saveToRedis(formDataObject);
 
   // TODO errors bellow caused by possible null values validation can fix
-  //   return await createCharacter(
-  //     {
-  //       characterName,
-  //       characterClass,
-  //       alignment,
-  //       level,
-  //       deity,
-  //       homeland,
-  //       race,
-  //       size,
-  //       gender,
-  //       characterAge,
-  //       characterHeight,
-  //       weight,
-  //       hairColor,
-  //       eyeColor,
-  //       strength,
-  //       dexterity,
-  //       constitution,
-  //       intelligence,
-  //       wisdom,
-  //       charisma,
-  //     },
-  //     request
-  //   );
+  return await createCharacter(
+    {
+      ...values,
+      // values.characterName,
+      // values.characterClass,
+      // values.alignment,
+      // values.level,
+      // values.deity,
+      // values.homeland,
+      // values.race,
+      // values.size,
+      // values.gender,
+      // values.characterAge,
+      // values.characterHeight,
+      // values.weight,
+      // values.hairColor,
+      // values.eyeColor,
+      // values.strength,
+      // values.dexterity,
+      // values.constitution,
+      // values.intelligence,
+      // values.wisdom,
+      // values.charisma,
+    },
+    request
+  );
 };
 const Creation = () => {
   const [characterData, setCharacterData] = useState<CharacterForm | null>(
@@ -144,20 +151,25 @@ const Creation = () => {
     wisdom: actionData?.fields?.wisdom || "",
     charisma: actionData?.fields?.charisma || "",
   });
+
+  const submit = useSubmit();
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    submit(formData, { method: "post" });
+  };
   const handleChange = (e: ChangeEvent<HTMLInputElement>, field: string) => {
     setFormData((form) => ({
       ...form,
       [field]: e.target.value,
     }));
   };
-
   //   Stepper Functions
   const [active, setActive] = useState(0);
   const nextStep = () => {
     setActive((current) => (current < 5 ? current + 1 : current));
-    setCharacterData({ ...formData });
+    // setCharacterData({ ...formData });
   };
-  console.log("characterData", characterData);
+  // console.log("characterData", characterData);
   const prevStep = () =>
     setActive((current) => (current > 0 ? current - 1 : current));
   return (
@@ -212,10 +224,6 @@ const Creation = () => {
                 border: "1px solid red",
                 width: "40%",
                 height: "100%",
-                //   display: "flex",
-                //   flexDirection: "row-reverse",
-                //   justifyContent: "center",
-                //   alignItems: "center",
               }}
             >
               <Container
@@ -227,7 +235,7 @@ const Creation = () => {
                   color: "white",
                 }}
               >
-                <Form method="post">
+                <Form onSubmit={handleSubmit}>
                   {active === 0 ? (
                     <>
                       <Title color="white" align="center">
@@ -369,7 +377,7 @@ const Creation = () => {
                             htmlFor="strength"
                             label="Strength"
                             value={formData.strength}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "strength")}
                           />
@@ -377,7 +385,7 @@ const Creation = () => {
                             htmlFor="dexterity"
                             label="Dexterity"
                             value={formData.dexterity}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "dexterity")}
                           />
@@ -385,7 +393,7 @@ const Creation = () => {
                             htmlFor="constitution"
                             label="Constitution"
                             value={formData.constitution}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "constitution")}
                           />
@@ -401,7 +409,7 @@ const Creation = () => {
                             htmlFor="intelligence"
                             label="Intelligence"
                             value={formData.intelligence}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "intelligence")}
                           />
@@ -409,7 +417,7 @@ const Creation = () => {
                             htmlFor="wisdom"
                             label="Wisdom"
                             value={formData.wisdom}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "wisdom")}
                           />
@@ -417,7 +425,7 @@ const Creation = () => {
                             htmlFor="charisma"
                             label="Charisma"
                             value={formData.charisma}
-                            type="number"
+                            type="string"
                             style={{ width: "51%" }}
                             onChange={(e) => handleChange(e, "charisma")}
                           />
@@ -451,6 +459,8 @@ const Creation = () => {
                   ) : null}
                   {active < 5 && (
                     <Button
+                      name="_action"
+                      value={active === 5 ? "submit" : `step${active + 1}`}
                       onClick={() => nextStep()}
                       style={{ marginTop: "10%" }}
                     >
