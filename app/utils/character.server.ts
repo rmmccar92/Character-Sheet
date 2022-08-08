2ea1a8a3e1912e44bab97337cae6cde2ebbfcfbb
Ryan McCarthy <rmmcc92@aol.com>
2022-08-08 19:52:43 -0400

    characters now being created on db have to figure out TS warning

--- app/utils/character.server.ts
+++ app/utils/character.server.ts
@@ -4,17 +4,36 @@ import { getUserSession } from "./auth.server";
 
 // TODO: Character creation
 
-export const createCharacter = async (character: any, request: Request) => {
+export const createCharacter = async (
+  character: CharacterForm,
+  request: Request
+) => {
   const session = await getUserSession(request);
   const userId = session.get("userId");
   // console.log("userId", userId);
-  console.log("character", character);
-  return null;
-  // const newCharacter = await prisma.character.create({
-  //   data: {
-  //     user: will need context to get user id
-  //     character: {
-  //       ...character,
-  //   },
-  // });
+  // console.log("character", character);
+  const newCharacter = await prisma.character.create({
+    data: {
+      userId: userId,
+      characterName: character.characterName,
+      class: character.characterClass,
+      // alignment: character.alignment,
+      // level: character.level,
+      // deity: character.deity,
+      home: character.homeland,
+      // race: character.race,
+      // size: character.size,
+      gender: character.gender,
+      // characterAge: character.characterAge
+      stats: {
+        strength: parseInt(character?.strength),
+        dexterity: parseInt(character?.dexterity),
+        constitution: parseInt(character?.constitution),
+        wisdom: parseInt(character?.wisdom),
+        intelligence: parseInt(character?.intelligence),
+        charisma: parseInt(character?.charisma),
+      },
+    },
+  });
+  return { id: newCharacter.id, characterName: newCharacter.characterName };
 };
--- app/utils/types.server.ts
+++ app/utils/types.server.ts
@@ -10,25 +10,24 @@ export interface LoginForm {
   password: string;
 }
 export interface CharacterForm {
-  id: string;
-  characterName: string;
-  characterClass: string;
-  alignment: string;
-  level: number;
-  deity: string;
-  homeland: string;
-  race: string;
-  size: string;
-  gender: string;
-  characterAge: string;
+  characterName?: string;
+  characterClass?: string;
+  alignment?: string;
+  level?: number;
+  deity?: string;
+  homeland?: string;
+  race?: string;
+  size?: string;
+  gender?: string;
+  characterAge?: string;
   characterHeight?: string;
-  weight: string;
+  weight?: string;
   hairColor?: string | null;
   eyeColor?: string;
-  strength: number | string;
-  dexterity: number | string;
-  constitution: number | string;
-  wisdom: number | string;
-  intelligence: number | string;
-  charisma: number | string;
+  strength?: number | string;
+  dexterity?: number | string;
+  constitution?: number | string;
+  wisdom?: number | string;
+  intelligence?: number | string;
+  charisma?: number | string;
 }
