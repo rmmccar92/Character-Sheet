b69b8d13c108e2760c7687f2a0a73e224003f548
Ryan McCarthy <rmmcc92@aol.com>
2022-08-27 21:43:04 -0400

    more fields in defense

--- app/components/Creation/Defense.tsx
+++ app/components/Creation/Defense.tsx
@@ -1,9 +1,31 @@
+import { Text } from "@mantine/core";
 import type { FC } from "react";
 import { FormField } from "~/components/form-field";
 
 interface DefenseProps {}
 
 const Defense: FC<DefenseProps> = ({}) => {
-  return <FormField htmlFor="Defense" value={"defense"} type="text" />;
+  return (
+    <>
+      <FormField htmlFor="ac" value={"AC"} type="text" />
+      <FormField htmlFor="touch" value={"Touch"} type="text" />
+      <FormField htmlFor="flatFooted" value={"Flat-Footed"} type="text" />
+      <FormField htmlFor="hp" value={"HP"} type="text" />
+      <FormField htmlFor="dr" value={"dr"} type="text" />
+      <FormField htmlFor="sr" value={"sr"} type="text" />
+      <Text>
+        <strong>Saving Throws</strong>
+      </Text>
+      <FormField htmlFor="fortitude" value={"fortitude"} type="text" />
+      <FormField htmlFor="will" value={"Will"} type="text" />
+      <FormField htmlFor="reflex" value={"Reflex"} type="text" />
+      <Text>
+        <strong>Other</strong>
+      </Text>
+      <FormField htmlFor="resistances" value={"Resistances"} type="text" />
+      <FormField htmlFor="immunities" value={"Immunities"} type="text" />
+      <FormField htmlFor="cmd" value={"Combat Maneuver Defense"} type="text" />
+    </>
+  );
 };
 export default Defense;
