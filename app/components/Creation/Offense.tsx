bb44d6ae85329af04b2380ed3cba78a9fc6e7320
Ryan McCarthy <rmmcc92@aol.com>
2022-08-27 21:46:52 -0400

    more fields on offense

--- app/components/Creation/Offense.tsx
+++ app/components/Creation/Offense.tsx
@@ -4,6 +4,13 @@ import { FormField } from "~/components/form-field";
 interface OffenseProps {}
 
 const Offense: FC<OffenseProps> = ({}) => {
-  return <FormField htmlFor="Offense" value={"offense"} type="text" />;
+  return (
+    <>
+      <FormField htmlFor="intiative" value={"initiative"} type="text" />
+      <FormField htmlFor="bab" value={"Base Attack Bonus"} type="text" />
+      <FormField htmlFor="speed" value={"Speed"} type="text" />
+      <FormField htmlFor="cmb" value={"Combat Maneuver Bonus"} type="text" />
+    </>
+  );
 };
 export default Offense;
