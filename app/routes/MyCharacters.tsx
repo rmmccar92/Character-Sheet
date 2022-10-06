8a80a9a5bd7c5b045215d43c45cb159889712da8
Ryan McCarthy <rmmcc92@aol.com>
2022-10-05 21:04:35 -0400

    started styling roster page

--- app/routes/MyCharacters.tsx
+++ app/routes/MyCharacters.tsx
@@ -4,7 +4,13 @@ import { getAllCharacters } from "~/utils/character.server";
 import { json } from "@remix-run/node";
 import { requireUserId } from "~/utils/auth.server";
 import { useLoaderData } from "@remix-run/react";
-import { Title } from "@mantine/core";
+import { Container, Title } from "@mantine/core";
+import Layout from "~/components/Layout";
+import styles from "../styles/roster.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -17,14 +23,23 @@ interface MyCharactersProps {}
 const MyCharacters: FC<MyCharactersProps> = ({}) => {
   const { characters } = useLoaderData();
   return (
-    <div>
+    <Layout>
       <Title>Characters</Title>
-      {characters.map((character: any) => (
-        <div key={character.id}>
-          <h2>{character.characterName}</h2>
-        </div>
-      ))}
-    </div>
+      <Container className="card-container">
+        {characters.length > 0 ? (
+          <>
+            {characters.map((character: any) => (
+              <div className="character-card" key={character.id}>
+                <h2>{character.characterName}</h2>
+              </div>
+            ))}
+          </>
+        ) : (
+          <Title>Roster Empty Click Here To Make a New Character</Title>
+        )}
+      </Container>
+    </Layout>
   );
 };
+
 export default MyCharacters;
