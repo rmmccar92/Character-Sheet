12e9dea5b6d4014bde1394057a231f410564a4ef
Ryan McCarthy <rmmcc92@aol.com>
2022-10-07 19:49:58 -0400

    getting ready for next step

--- app/routes/upload-image.ts
+++ app/routes/upload-image.ts
@@ -1,10 +1,11 @@
-import { ActionFunction, json } from "@remix-run/node";
-import { requireUserId } from "~/utils/auth.server";
+import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+// import { requireUserId } from "~/utils/auth.server";
 import { uploadImage } from "~/utils/s3.server";
 import { prisma } from "~/utils/prisma.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const userId = await requireUserId(request);
+  //   const userId = await requireUserId(request);
 
   const characterId = request.headers.get("characterId");
 
