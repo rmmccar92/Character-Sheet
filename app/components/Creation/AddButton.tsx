47d0ced6348bed8c11937bb5558b03d991f2ca2c
Ryan McCarthy <rmmcc92@aol.com>
2022-10-16 17:19:55 -0400

    more modal styling

--- app/components/Creation/AddButton.tsx
+++ app/components/Creation/AddButton.tsx
@@ -30,60 +30,32 @@ const AddButton: FC<AddButtonProps> = ({ type, handleChange, formData }) => {
           setIsOpen={setIsOpen}
           type={type}
         >
-          <Title>{`Add ${type}`}</Title>
-          {/* {type === "feat" ? ( */}
-          <>
-            <FormField
-              htmlFor="name"
-              type="text"
-              label={`${
-                type.charAt(0).toUpperCase() + type.substring(1).slice(0, -1)
-              } Name`}
-              value={formData[type].name}
-              style={{ width: "100%", margin: "2% 0 2% 0" }}
-              onChange={(e) => handleChange(e, `${type}`, "name")}
-              className="add-input"
-            />
-            <FormField
-              htmlFor="description"
-              type="textarea"
-              label={`${
-                type.charAt(0).toUpperCase() + type.substring(1).slice(0, -1)
-              } Description`}
-              value={formData[type].description}
-              style={{ height: "15em", width: "100%" }}
-              onChange={(e) => handleChange(e, `${type}`, "description")}
-              className="add-textarea"
-            />
-            <Button type="submit" style={{ marginTop: "40%" }}>
-              Submit
-            </Button>
-          </>
-          {/* ) : type === "trait" ? (
-            <>
+          <div className="modal-inner">
+            <Title className="add-title">{`Add ${type}`}</Title>
+            <div className="modal-forms">
               <FormField
                 htmlFor="name"
                 type="text"
-                label="Trait Name"
-                value={formData.traits.name}
-                style={{ width: "100%", margin: "2% 0 2% 0" }}
-                onChange={(e) => handleChange(e, "traits", "name")}
+                label={`${
+                  type.charAt(0).toUpperCase() + type.substring(1).slice(0, -1)
+                } Name`}
+                value={formData[type].name}
+                onChange={(e) => handleChange(e, `${type}`, "name")}
                 className="add-input"
               />
               <FormField
                 htmlFor="description"
                 type="textarea"
-                label="Trait Description"
-                value={formData.traits.description}
-                style={{ height: "15em", width: "100%" }}
-                onChange={(e) => handleChange(e, "traits", "description")}
+                label={`${
+                  type.charAt(0).toUpperCase() + type.substring(1).slice(0, -1)
+                } Description`}
+                value={formData[type].description}
+                onChange={(e) => handleChange(e, `${type}`, "description")}
                 className="add-textarea"
               />
-              <Button type="submit" style={{ marginTop: "40%" }}>
-                Submit
-              </Button>
-            </>
-          ) : null} */}
+            </div>
+            <Button type="submit">Submit</Button>
+          </div>
         </Modal>
       )}
       <Button
